09b204172059dd0fd8e6a33fa8c271118f7141e7
semaaJ <mamesjiles@gmail.com>
2019-10-10 01:08:41 +0100

    basic tree, state and general file layout complete

--- src/app/src/components/general/TreeGraph/__tests__/TreeGraph.test.js
+++ src/app/src/components/general/TreeGraph/__tests__/TreeGraph.test.js
@@ -1,19 +1,64 @@
 import React from 'react';
 import TreeGraph from '../TreeGraph';
-import initialState from '../../../../store/tree/tree';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import "../../../../setupTests"
 
-describe('TreeGraph', () => {
-    test('Should render TreeGraph succesfully', () => { 
-        const component = shallow(<TreeGraph tree={initialState} />);
-        expect(component).toMatchSnapshot();
-    });
-    test('Should call hideAllChildNodes on node click', () => {
-        const hideAllChildNodes = jest.fn();
-        const component = shallow(<TreeGraph tree={initialState} hideAllChildNodes={hideAllChildNodes} />);
-        const node = component.find('#TreeGraph.js');
-        node.simulate('click');
-        expect(hideAllChildNodes).toHaveBeenCalled();
-    });
-})
+const initialState = {
+    name: '/src',
+    children: [{
+        name: 'TreeGraph.js',
+        gProps: {
+            className: 'class',
+            id: 'TreeGraph.js'
+        },
+        children: [{
+            name: 'arguments',
+            gProps: {
+                className: 'variable',
+                id: 'arguments'
+            },
+            pathProps: {
+                className: 'class-link'
+            },
+            children: [{
+                name: 'Int arg1',
+                gProps: {
+                    className: 'variable',
+                    id: 'Int arg1'
+                },
+                pathProps: {
+                    className: 'variable-link'
+                }
+            }, {
+                name: 'Int arg2',
+                gProps: {
+                    className: 'variable',
+                    id: 'Int arg2'
+                },
+                pathProps: {
+                    className: 'variable-link'
+                }
+            }]
+        }, {
+            name: 'functions',
+            pathProps: {
+                className: 'class-link'
+            },
+            children: [{
+                name: 'square -> Int arg1, Int arg2'
+            }, {
+                name: 'sum -> Int arg1, Int arg2'
+            }]
+        }]
+    }, {
+        name: 'TreeGraph.css',
+        gProps: {
+            className: 'variable'
+        }
+    }]
+}
+
+test('Should render TreeGraph succesfully', () => { 
+    const component = mount(<TreeGraph tree={initialState} />);
+    expect(component.html()).toMatchSnapshot();
+});
--- src/app/src/store/tree/tree.js
+++ src/app/src/store/tree/tree.js
@@ -1,57 +1,57 @@
-
-export const initialState = {
-        name: '/src',
+const initialState = {
+    name: '/src',
+    children: [{
+        name: 'TreeGraph.js',
+        gProps: {
+            className: 'class',
+            id: 'TreeGraph.js',
+            'data-testid': 'TreeGraph.js'
+        },
         children: [{
-            name: 'TreeGraph.js',
+            name: 'arguments',
             gProps: {
-                className: 'class',
-                id: 'TreeGraph.js'
+                className: 'variable',
+                id: 'arguments'
+            },
+            pathProps: {
+                className: 'class-link'
             },
             children: [{
-                name: 'arguments',
+                name: 'Int arg1',
                 gProps: {
                     className: 'variable',
-                    id: 'arguments'
+                    id: 'Int arg1'
                 },
                 pathProps: {
-                    className: 'class-link'
-                },
-                children: [{
-                    name: 'Int arg1',
-                    gProps: {
-                        className: 'variable',
-                        id: 'Int arg1'
-                    },
-                    pathProps: {
-                        className: 'variable-link'
-                    }
-                }, {
-                    name: 'Int arg2',
-                    gProps: {
-                        className: 'variable',
-                        id: 'Int arg2'
-                    },
-                    pathProps: {
-                        className: 'variable-link'
-                    }
-                }]
+                    className: 'variable-link'
+                }
             }, {
-                name: 'functions',
-                pathProps: {
-                    className: 'class-link'
+                name: 'Int arg2',
+                gProps: {
+                    className: 'variable',
+                    id: 'Int arg2'
                 },
-                children: [{
-                    name: 'square -> Int arg1, Int arg2'
-                }, {
-                    name: 'sum -> Int arg1, Int arg2'
-                }]
+                pathProps: {
+                    className: 'variable-link'
+                }
             }]
         }, {
-            name: 'TreeGraph.css',
-            gProps: {
-                className: 'variable'
-            }
+            name: 'functions',
+            pathProps: {
+                className: 'class-link'
+            },
+            children: [{
+                name: 'square -> Int arg1, Int arg2'
+            }, {
+                name: 'sum -> Int arg1, Int arg2'
+            }]
         }]
+    }, {
+        name: 'TreeGraph.css',
+        gProps: {
+            className: 'variable'
+        }
+    }]
 }
 
 export const HIDE_ALL_CHILD_NODES = 'HIDE_ALL_CHILD_NODES';

ebe0177d7f63158f06f45c409fc8f96aaeab49f4
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2020-02-11 12:20:39 +0000

    Implementing API "views" (endpoints).

--- src/server/api/models.py
+++ src/server/api/models.py
@@ -18,6 +18,9 @@ class File(models.Model):
     when_uploaded = models.DateTimeField(auto_now=True)
     file = models.FileField(upload_to=uploaded_file_path, unique=True)
 
+    def __str__(self):
+        return self.name
+
     def save(self, **kwargs):
         super().save(**kwargs)
 
@@ -27,6 +30,9 @@ class Class(models.Model):
     file_hash = models.ForeignKey("File", on_delete=models.CASCADE)
     name = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Method(models.Model):
     hash = models.CharField(max_length=64, primary_key=True, editable=False)
@@ -35,14 +41,23 @@ class Method(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     return_type = models.CharField(max_length=255, default="void")
 
+    def __str__(self):
+        return self.name
+
 
 class MethodParameter(models.Model):
     method_hash = models.ForeignKey("Method", on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255, default="")
 
+    def __str__(self):
+        return self.name
+
 
 class ClassRelationship(models.Model):
     parent_hash = models.ForeignKey("Class", on_delete=models.CASCADE, related_name="+")
     child_hash = models.ForeignKey("Class", on_delete=models.CASCADE, related_name="+")
     relationship_type = models.CharField(max_length=255, default="association")
+
+    def __str__(self):
+        return self.name

de754951eb5fcc9cd5fe5a81658d665311212979
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2020-02-25 12:16:41 +0000

    Removing rest_framework_swagger from INSTALLED_APPS.

--- src/server/server/settings/default.py
+++ src/server/server/settings/default.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_swagger',
     'rest_framework',
     'api.apps.ApiConfig',
 ]

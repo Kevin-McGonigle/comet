b56f4c28ebb90932fbf073d49e698b84a9ac0ada
semaaJ <mamesjiles@gmail.com>
2020-03-01 15:45:35 +0000

    feature: method & class viewpoints - all viewpoints now return all stored data on empty get request

--- src/server/api/serializers.py
+++ src/server/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import File
+from .models import File, Method, Class
 
 class FileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = File
         fields = ['hash', 'name', 'size', 'file_type', 'when_uploaded', 'file']
+
+class MethodSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Method
+        fields = ['method_hash', 'parent', 'name', 'arguments', 'returns', 'child_hash']
+
+class ClassSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Class
+        fields = ['class_hash', 'parent', 'name', 'arguments', 'methods', 'returns', ' child_hash']
--- src/server/api/views.py
+++ src/server/api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from rest_framework import viewsets, status
-from .models import File
+from .models import File, Method, Class
 from . import serializers
 import json
 
@@ -26,8 +26,43 @@ class FileInformationViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.method == 'GET':
             file_hash = self.request.GET.get('hash', None)
-            queryset = File.objects.all().filter(hash=file_hash)
-            
             if file_hash is not None:
-                return queryset
+                return File.objects.all().filter(hash=file_hash)
+
+            return File.objects.all()
             
+
+class MethodViewpoint(viewsets.ModelViewSet):
+    """
+    API endpoint to return a method's information from a given hash
+    """
+    serializer_class = serializers.MethodSerializer
+
+    def get_queryset(self):
+        if self.request.method == "GET":
+            method_hash = self.request.GET.get('method_hash', None)
+            if method_hash is not None:
+                return Method.objects.all().filter(method_hash=method_hash)
+            
+            return Method.objects.all()
+
+
+class ClassViewpoint(viewsets.ModelViewSet):
+    """
+    API endpoint to return a class' information from a given hash
+    """
+    serializer_class = serializers.ClassSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return JsonResponse({'hash': serializer.data['class_hash']}, status=status.HTTP_201_CREATED, safe=False)
+
+    def get_queryset(self):
+        if self.request.method == "GET":
+            class_hash = self.request.GET.get('class_hash', None)
+            if class_hash is not None:
+                return Class.objects.all().filter(class_hash=class_hash)
+            
+            return Class.objects.all()

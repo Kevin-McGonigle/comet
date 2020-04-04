3d70827d96996666fa4913ec1123c6ed85ecf110
semaaJ <mamesjiles@gmail.com>
2020-04-04 18:10:23 +0100

    inheritance tree & tests

---
+++ src/metrics/parsers/helpers/tests/inheritance_node_tests.py
@@ -0,0 +1,33 @@
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+import unittest
+from inheritance_tree import InheritanceNode
+from comet_visitor import CometNodeResult
+
+class InheritanceTreeTestCase(unittest.TestCase):
+    CLASS_A_TOKENS = ['class', 'A', '(', ')', ':', '\n', 'def', 'test', '(', ')', ':', '\n', 'pass', '\n', '\n']
+    CLASS_B_TOKENS = ['class', 'B', '(', 'A', ')', ':', '\n', 'def', 'test_two', '(', 'self', ',', 'x', ',', 'y', ')', '->', CometNodeResult('int', None, None), ':', '\n', 'pass', '\n']
+    CLASS_C_TOKENS = ['class', 'C', '(','A', ',' ,'B', ')', ':', '\n', '    ', 'pass', '\n', '\n']
+
+    def setUp(self):
+        self.a_node = InheritanceNode(self.CLASS_A_TOKENS)     
+        self.b_node = InheritanceNode(self.CLASS_B_TOKENS)     
+        self.c_node = InheritanceNode(self.CLASS_C_TOKENS)
+
+    def testCorrectParsingOfClassArguments(self):
+        assert(self.a_node.class_name == "A" and self.a_node.parent == [])
+        assert(self.a_node.methods == {"test": {"args": []}})
+
+        assert(self.b_node.class_name == "B" and self.b_node.parent == ["A"])
+        assert(self.b_node.methods == {"test_two": {"args": ["self", "x", "y"], "return": 'int'}})
+
+        assert(self.c_node.class_name == "C" and self.c_node.parent == ["A", "B"])
+        assert(self.c_node.methods == {})
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/metrics/parsers/helpers/tests/inheritance_tree_tests.py
+++ src/metrics/parsers/helpers/tests/inheritance_tree_tests.py
@@ -5,61 +5,42 @@ sys.path.insert(0, parentdir)
 
 import unittest
 from inheritance_tree import InheritanceTree, InheritanceNode
+from comet_visitor import CometNodeResult
 
 class InheritanceTreeTestCase(unittest.TestCase):
-    CLASS_A_TOKENS = ['class', 'A', '(', ')', ':', '\n', '    ', 'pass', '\n', '\n']
-    CLASS_B_TOKENS = ['class', 'B', '(', 'A', ')', ':', '\n', '    ', 'pass', '\n', '\n']
-    CLASS_C_TOKENS = ['class', 'C', '(','A', ',' ,'B', ')', ':', '\n', '    ', 'pass', '\n', '\n']
+    CLASS_A_TOKENS = ['class', 'A', '(', ')', ':', '\n', 'def', 'test', '(', ')', ':', '\n', 'pass', '\n', '\n']
+    CLASS_B_TOKENS = ['class', 'B', '(', CometNodeResult('A', None, None), ')', ':', '\n', 'def', 'test_two', '(', 'x', ',', 'y', ')', ':', '\n', 'pass', '\n']
+    CLASS_C_TOKENS = ['class', 'C', '(', CometNodeResult('A', None, None), ',', CometNodeResult('B', None, None), ')', ':', '\n', '    ', 'pass', '\n', '\n']
 
     def setUp(self):
         self.inheritance_tree = InheritanceTree()
         self.a_node = InheritanceNode(self.CLASS_A_TOKENS)     
         self.b_node = InheritanceNode(self.CLASS_B_TOKENS)     
         self.c_node = InheritanceNode(self.CLASS_C_TOKENS)
-
-    def testCorrectParsingOfClassArguments(self):
-        assert(self.a_node.class_name == "A" and self.a_node.parent == [])
-        assert(self.b_node.class_name == "B" and self.b_node.parent == ["A"])
-        assert(self.c_node.class_name == "C" and self.c_node.parent == ["A", "B"])
-
+    
     def testAddNode(self):
-        assert(len(self.inheritance_tree.root.values()) == 0)
+        self.assertEqual(len(self.inheritance_tree.root.values()), 0)
         self.inheritance_tree.add_node(self.a_node)
-        assert(len(self.inheritance_tree.root.values()) > 0)
-        assert(self.inheritance_tree.root[self.a_node] != None)
-
-    def testGenerateCorrectInheritanceTree(self):
-        self.inheritance_tree.add_node(self.a_node)
-        self.inheritance_tree.add_node(self.b_node)
-        self.inheritance_tree.add_node(self.c_node)
-
-        assert(self.inheritance_tree.root[self.a_node] == [])
-        assert(self.inheritance_tree.root[self.b_node] == ['A'])
-        assert(self.inheritance_tree.root[self.c_node] == ['A', 'B'])
-
-    def testGetChidren(self):
-        self.inheritance_tree.add_node(self.a_node)
-        self.inheritance_tree.add_node(self.b_node)
-
-        a_children = self.inheritance_tree.get_children(self.a_node) 
-        b_children = self.inheritance_tree.get_children(self.b_node)
-
-        assert(a_children == ['B'])
-        assert(b_children == [])
+        self.assertEqual(len(self.inheritance_tree.root.values()), 1)
+        self.assertNotEqual(self.inheritance_tree.root[self.a_node], None)
 
     def testGetParentNode(self):
         self.inheritance_tree.add_node(self.a_node)
         self.inheritance_tree.add_node(self.b_node)
         self.inheritance_tree.add_node(self.c_node)
 
-        a_parent = self.inheritance_tree.get_parent_node(self.a_node)
-        b_parent = self.inheritance_tree.get_parent_node(self.b_node)
-        c_parent = self.inheritance_tree.get_parent_node(self.c_node)
+        self.assertEqual(self.inheritance_tree.get_parent_node(self.a_node), [])
+        self.assertEqual(self.inheritance_tree.get_parent_node(self.b_node), ['A'])
+        self.assertEqual(self.inheritance_tree.get_parent_node(self.c_node), ['A', 'B'])
 
-        assert(a_parent == [])
-        assert(b_parent == ['A'])
-        assert(c_parent == ['A', 'B'])
+    def testGetChildren(self):
+        self.inheritance_tree.add_node(self.a_node)
+        self.inheritance_tree.add_node(self.b_node)
+        self.inheritance_tree.add_node(self.c_node)
 
+        self.assertEqual(self.inheritance_tree.get_children(self.a_node), ['B', 'C'])
+        self.assertEqual(self.inheritance_tree.get_children(self.b_node), ['C'])
+        self.assertEqual(self.inheritance_tree.get_children(self.c_node), [])    
 
 if __name__ == "__main__":
     unittest.main()

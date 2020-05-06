5b0375c7196679bd92a758a2af51c51f613d987d
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2020-05-06 17:59:57 +0100

    Calculator implementation.

--- src/server/metrics/calculator.py
+++ src/server/metrics/calculator.py
@@ -1,52 +1,120 @@
+from typing import Optional
+
 from metrics.structures.ast import AST
 from metrics.structures.cfg import CFG
 from metrics.structures.inheritance_tree import InheritanceTree
+from visitors.metrics.lloc_calculation_visitor import LLOCCalculationVisitor
+from visitors.structures.cfg_generation_visitor import CFGGenerationVisitor
+from visitors.structures.inheritance_tree_generation_visitor import InheritanceTreeGenerationVisitor
 
 
 class Calculator(object):
-    def __init__(self, ast=None, cfg=None, inheritance_tree=None):
+    """
+    Metric/model calculator.
+
+    Class for calculating metrics and generating models for a given AST.
+    """
+
+    def __init__(self, ast: AST = None):
         """
         Metric/model calculator.
+
         :param ast: Abstract syntax tree.
-        :type ast: AST
-        :param cfg: Control flow graph.
-        :type cfg: CFG or None
-        :param inheritance_tree: Inheritance tree.
-        :type inheritance_tree: InheritanceTree or None
         """
-        self.ast = ast
-        self.cfg = cfg
-        self.it = inheritance_tree
+        self.__ast = None
 
-    def abstract_syntax_tree(self):
-        if self.ast:
-            return self.ast
+        # TODO: Maybe substitute the below for a single dictionary for extensibility and easier clearing.
+        self.cd = None
+        self.cfg = None
+        self.it = None
+        self.dg = None
 
-        # TODO: Generate AST
+    # region ast Property
 
-    def control_flow_graph(self):
+    @property
+    def ast(self) -> Optional[AST]:
+        """
+        Getter for ast property.
+
+        :return: Value of ast.
+        """
+        return self.__ast
+
+    @ast.setter
+    def ast(self, new_ast: AST):
+        """
+        Setter for ast property. Also clears all accompanying structures.
+
+        :param new_ast: Value to assign to ast.
+        """
+        self.clear()
+        self.__ast = new_ast
+
+    @ast.deleter
+    def ast(self):
+        """
+        Deleter for ast property.
+        """
+        del self.__ast
+
+    # endregion
+
+    # region Helpers
+
+    def clear(self):
+        self.__ast = None
+        self.cd = None
+        self.cfg = None
+        self.it = None
+        self.dg = None
+
+    # endregion
+
+    # region Models
+
+    def control_flow_graph(self, ast: Optional[AST]) -> CFG:
         """
         Calculate control flow graph.
-        :return: Control flow graph.
-        :rtype: CFG
+        :param ast: Abstract syntax tree to generate CFG from.
+        :return: The corresponding control-flow graph.
         """
-        if self.cfg:
-            return self.cfg
+        if ast:
+            return CFGGenerationVisitor().visit(ast)
 
-        # TODO: Generate CFG
+        if not self.cfg:
+            self.cfg = CFGGenerationVisitor().visit(self.ast)
 
-        pass
+        return self.cfg
 
-    def inheritance_tree(self):
+    def inheritance_tree(self, ast: Optional[AST]) -> InheritanceTree:
         """
         Calculate inheritance tree.
-        :return: Inheritance tree.
-        :rtype: InheritanceTree
+        :param ast: Abstract syntax tree to generate inheritance tree from.
+        :return: The corresponding inheritance tree.
         """
-        if self.it:
-            return self.it
+        if ast:
+            return InheritanceTreeGenerationVisitor().visit(ast)
 
-        # TODO: Generate inheritance tree
-        pass
+        if not self.it:
+            self.it = InheritanceTreeGenerationVisitor().visit(self.ast)
 
-    # TODO: Add calculation functions for all metrics and structures.
+        return self.it
+
+    # endregion
+
+    # region Metrics
+
+    def logical_lines_of_code(self, ast: Optional[AST]) -> int:
+        """
+        Calculate logical lines of code.
+        :param ast: Abstract syntax tree to calculate logical lines of code from.
+        :return: The corresponding logical lines of code.
+        """
+        if ast:
+            return LLOCCalculationVisitor().visit(ast)
+
+        return LLOCCalculationVisitor().visit(self.ast)
+
+    # endregion
+
+    # TODO: Add calculation functions for remaining metrics and structures.

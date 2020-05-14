cdb210d2ce4ed38a3d7aded887702cc2a5132246
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2020-05-14 13:57:15 +0100

    CFG unit testing.

---
+++ src/server/metrics/structures/tests/test_cfg.py
@@ -0,0 +1,205 @@
+from unittest import TestCase
+
+
+class TestCFG(TestCase):
+    def test_entry_block_getter(self):
+        self.fail()
+
+    def test_entry_block_setter(self):
+        self.fail()
+
+    def test_entry_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+
+class TestCFGBlock(TestCase):
+    def test_exit_block_getter(self):
+        self.fail()
+
+    def test_exit_block_setter(self):
+        self.fail()
+
+    def test_exit_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+
+class TestCFGIfBlock(TestCase):
+    def test_success_block_getter(self):
+        self.fail()
+
+    def test_success_block_setter(self):
+        self.fail()
+
+    def test_success_block_deleter(self):
+        self.fail()
+
+    def test_exit_block_getter(self):
+        self.fail()
+
+    def test_exit_block_setter(self):
+        self.fail()
+
+    def test_exit_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGIfElseBlock(TestCase):
+    def test_success_block_getter(self):
+        self.fail()
+
+    def test_success_block_setter(self):
+        self.fail()
+
+    def test_success_block_deleter(self):
+        self.fail()
+
+    def test_fail_block_getter(self):
+        self.fail()
+
+    def test_fail_block_setter(self):
+        self.fail()
+
+    def test_fail_block_deleter(self):
+        self.fail()
+
+    def test_exit_block_getter(self):
+        self.fail()
+
+    def test_exit_block_setter(self):
+        self.fail()
+
+    def test_exit_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGLoopBlock(TestCase):
+    def test_success_block_getter(self):
+        self.fail()
+
+    def test_success_block_setter(self):
+        self.fail()
+
+    def test_success_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGLoopElseBlock(TestCase):
+    def test_success_block_getter(self):
+        self.fail()
+
+    def test_success_block_setter(self):
+        self.fail()
+
+    def test_success_block_deleter(self):
+        self.fail()
+
+    def test_fail_block_getter(self):
+        self.fail()
+
+    def test_fail_block_setter(self):
+        self.fail()
+
+    def test_fail_block_deleter(self):
+        self.fail()
+
+    def test_exit_block_getter(self):
+        self.fail()
+
+    def test_exit_block_setter(self):
+        self.fail()
+
+    def test_exit_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGSwitchBlock(TestCase):
+    def test_case_blocks_getter(self):
+        self.fail()
+
+    def test_case_blocks_setter(self):
+        self.fail()
+
+    def test_case_blocks_deleter(self):
+        self.fail()
+
+    def test_exit_block_getter(self):
+        self.fail()
+
+    def test_exit_block_setter(self):
+        self.fail()
+
+    def test_exit_block_deleter(self):
+        self.fail()
+
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGBreakBlock(TestCase):
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()
+
+
+class TestCFGContinueBlock(TestCase):
+    def test_accept(self):
+        self.fail()
+
+    def test_add_child(self):
+        self.fail()
+
+    def test_remove_child(self):
+        self.fail()

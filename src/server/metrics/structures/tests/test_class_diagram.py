1b9a6dcd45ab9b8aa28d7eef1890f2c175ff45ea
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2020-05-14 13:03:41 +0100

    Class diagram unit testing.

---
+++ src/server/metrics/structures/tests/test_class_diagram.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+
+
+class TestClassDiagram(TestCase):
+    """
+    Class diagram test case.
+    """
+
+    pass
+
+
+class TestRelationship(TestCase):
+    """
+    Relationship test case.
+    """
+
+    pass
+
+
+class TestClass(TestCase):
+    """
+    Class test case.
+    """
+
+    pass
+
+
+class TestMethod(TestCase):
+    """
+    Method test case.
+    """
+
+    pass
+
+
+class TestVariable(TestCase):
+    """
+    Variable test case.
+    """
+
+    pass
+
+
+class TestParameter(TestCase):
+    """
+    Parameter test case.
+    """
+
+    pass
+
+
+class TestAttribute(TestCase):
+    """
+    Attribute test case.
+    """
+
+    pass
